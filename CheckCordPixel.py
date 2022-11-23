1bf3c35a9938dd2603f0908aa3faf1f77a687b5a
Ivan <nazgoool1@gmail.com>
2022-11-23 11:56:27 +0400

    Readme correct

--- CheckCordPixel.py
+++ CheckCordPixel.py
@@ -1,7 +1,6 @@
 import cv2 as cv
 
 img = cv.imread('0202.jpg')
-
 def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:
         xy = "%d,%d" % (x, y)
@@ -9,8 +8,6 @@ def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
         cv.putText(img, xy, (x, y), cv.FONT_HERSHEY_PLAIN,
                     1.0, (0, 0, 0), thickness=1)
         cv.imshow("image", img)
-
-
 cv.namedWindow("image")
 cv.setMouseCallback("image", on_EVENT_LBUTTONDOWN)
 while 1:
--- DetectionOnVideo.py
+++ DetectionOnVideo.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2 as cv
 import argparse
-parser = argparse.ArgumentParser(description='This sample demonstrates the meanshift algorithm. \
-                                              The example file can be downloaded from: \
-                                              https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
-parser.add_argument('image', type=str, help='path to image file')
-args = parser.parse_args()
-cap = cv.VideoCapture(args.image)
+# parser = argparse.ArgumentParser(description='This sample demonstrates the meanshift algorithm. \
+#                                               The example file can be downloaded from: \
+#                                               https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
+# parser.add_argument('image', type=str, help='path to image file')
+# args = parser.parse_args()
+cap = cv.VideoCapture(0)
 # take first frame of the video
 ret, frame = cap.read()
 # setup initial location of window
